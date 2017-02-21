72e84fcd1e83ad2d1850d49eb50003cdd432e32f
Iurii Voitenko <jurgeban2@yandex.ua>
2017-02-21 23:08:38 +0200

    animation finished

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // document.createElement();getElementById();getElementsByTagName();getElementsByName();getElementsByClassName();querySelector();querySelectorAll();el.classList.add();el.innerHTML = "";e.style.background = '';el.appendChild();setInterval();setTimeout();clearInterval();
 
-putNewQuote(false);
+// putNewQuote(false);
+putNewQuote(true)
 
 
 
@@ -14,14 +15,19 @@ putNewQuote(false);
     var units = quotes[random][0].split(" ");
     if (bool) delAll();
     for (var i = 0; i < units.length; i++) {
-      var el = document.createElement('div');
-      el.classList.add('unit');
-      el.innerHTML = units[i];
-      document.getElementsByTagName('h3')[0].appendChild(el);
-      el.style.top = '-100px';
-      el.style.left = '50%';
+    	createElem('unit', units[i], document.getElementsByTagName('h3')[0])
     } 
-         replaceElems(bool);   
+      createElem('author', quotes[random][1], document.getElementsByTagName('h3')[0])
+      replaceElems(bool);   
+}
+//--------------------
+function createElem(Class, text, parent) {
+  var el = document.createElement('div');
+   el.classList.add(Class); 
+   el.innerHTML = text;
+   parent.appendChild(el);
+   el.style.top = '-100px';
+   el.style.left = '50%';
 }
  //------------------
 function delAll() {
@@ -33,24 +39,36 @@ function delAll() {
 function replaceElems(bool) {
 	  var left = -300;
 	  var units = document.querySelectorAll('.unit');
+	  var author = document.querySelectorAll('.author')[0];
+	  author.style.top = '-300px';
 	  for (var i = 0; i < units.length; i++) {
-	  	units[i].style.top = '-70%';
+	  	units[i].style.top = '-300px';
 	  	left += 170;
       units[i].style.left = left + 'px';
 	  }
   if(bool) {
-  	 left = 400, width = 0;
-      var timer = setTimeout(function() {
-      	for (var i = 0; i < units.length; i++) {
-      		left += ( width + 10 );
-      		units[i].style.top = '50%';
-      		units[i].style.left = left + 'px';
-      		var computedStyle = getComputedStyle(units[i]); 
-          width = computedStyle.width;
-          width = Number(width.substr(0, width.length - 2));
-      	}
-      }, 1);
-
+    animateElems(units, author);
   }
+}
+function animateElems(units, author) {
+	  	var left = 0, top1 = 0, width = 0, all = 0;
+      var timer = setTimeout(function() {
+      var computedH3 = getComputedStyle(document.getElementsByTagName('h3')[0]); 
+      var h3Width = computedH3.width;
+      h3Width = Number(h3Width.substr(0, h3Width.length - 2));
+      	for (var i = 0; i < units.length; i++) {
+      		if ( all >= h3Width - 70 ) {
+      		  left = 0, all = 0, width = 0, top1 += 30;
+      		}
+    		units[i].style.top = top1 + 'px';
+    		left += ( width + 10 );
+    		units[i].style.left = left + 'px';   		
+    		var computedStyle = getComputedStyle(units[i]); 
+        width = computedStyle.width;
+        width = Number(width.substr(0, width.length - 2));
+        all += width;
+      	}
+      	author.style.top = (top1 + 30) + 'px';
+      }, 1);
 }
  
